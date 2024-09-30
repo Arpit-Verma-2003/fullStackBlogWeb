1d1a044ee165f063ec670a885e97e1cb76be1b00
Arpit Verma <arpitverma200316@gmail.com>
2024-09-30 14:59:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://blog-frontend-ndqn.onrender.com"],
   methods: ["POST","GET","DELETE","PUT"],
   credentials: true 
 }));
